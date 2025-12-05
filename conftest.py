5e17267123010fb37bde27ba4f1b09ce6a1b1e70
Ekaterina-v8 <ekaterinafilatova022@gmail.com>
2025-12-05 18:33:29 -0500

    jobs_after_all_tests

--- conftest.py
+++ conftest.py
@@ -1,5 +1,31 @@
 import pytest
+import requests
 from playwright.sync_api import Playwright, ViewportSize
+
+api_token = "115962918aa4289dac16859b64ddac7e6e"
+user_name = "jen_admin"
+url = "http://localhost:8080/"
+
+
+
+def get_all_jobs():
+    response = requests.get(
+        url=f"{url}api/json",
+        auth=(user_name, api_token)
+    )
+    a = response.json()['jobs']
+    return response.json()['jobs']
+
+
+def delete_jobs():
+    jobs_list = get_all_jobs()
+
+    for job in jobs_list:
+        name = job["name"]
+        requests.post(
+            url=f"{url}/job/{name}/doDelete",
+            auth=(user_name, api_token)
+        )
 
 
 
@@ -17,6 +43,11 @@ def page(playwright: Playwright):
     page.close()
     context.close()
     browser.close()
+
+@pytest.fixture(scope="session", autouse=True)
+def delete_jobs_after_all_tests():
+    yield
+    delete_jobs()
 
 
 
