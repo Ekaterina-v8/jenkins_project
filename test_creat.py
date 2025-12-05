68b9e7f43dc4d387e036ec7774e3aa4084cc4ca7
Ekaterina-v8 <ekaterinafilatova022@gmail.com>
2025-12-05 12:46:37 -0500

    commit6

--- test_creat.py
+++
@@ -1,8 +0,0 @@
-
-
-def test_tc_01_001_01(page):
-    """Переходим по ссылке"""
-    page.goto("/")
-    username = "jen_admin"
-    password = "Ev08081981!!"
-    new_item_name = f"jen_admin-{random.randint(0, 9999999)}"
