1fd355d6f0fca9b06d937e0757bd94608f49c1d7
Ekaterina-v8 <ekaterinafilatova022@gmail.com>
2025-12-23 19:23:30 -0500

    feat: add test_folder_management.py

---
+++ data/dataclasses/create_user_dataclass.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class CreateUserDataClass:
+    username: str = None
+    password: str = None
+    confirm_password: str = None
+    fullname: str = None
+    email: str = None
